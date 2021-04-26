abdd6c7ace8b581608cfadc2f6e8c9199f8517da
kaganyasar <o.kagan_yasar@hotmail.com>
2021-04-26 20:29:38 +0300

    formatting

--- spring-cloud-contract-consumer/src/main/java/spring/cloud/contract/consumer/service/MessageRestController.java
+++ spring-cloud-contract-consumer/src/main/java/spring/cloud/contract/consumer/service/MessageRestController.java
@@ -2,6 +2,7 @@ package spring.cloud.contract.consumer.service;
 
 import data.Person;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ class MessageRestController {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    @RequestMapping("/message/{personId}")
-    String getMessage(@PathVariable("personId") Long personId) {
+    @GetMapping("/message/{personId}")
+    String getMessage(@PathVariable Long personId) {
         Person person = this.restTemplate.getForObject("http://localhost:8081/person/{personId}", Person.class, personId);
         return "Hello " + person.getName();
     }
